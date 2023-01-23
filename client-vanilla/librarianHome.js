f1cf97fc4aba78bdc46afde81f04520453a73fcc
Leandro Iannotti <leandroiannotti@outlook.com>
2023-01-23 18:51:58 +1300

    Client vanilla. React :( . Remove auth

---
+++ client-vanilla/librarianHome.js
@@ -0,0 +1,31 @@
+let searchType = "";
+
+function showSearchBox(type) {
+    searchType = type;
+    document.getElementById("search-container").style.display = "block";
+}
+
+function search() {
+    let searchValue = document.getElementById("search-input").value;
+    let url = "";
+
+    if (searchType === "book") {
+        url = `https://renti-library-api.herokuapp.com/book?title=${searchValue}`;
+    } else if (searchType === "author") {
+        url = `https://your-api.com/search/author?name=${searchValue}`;
+    }
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let searchResults = "";
+            data.forEach(item => {
+                searchResults += `<p>${item.name}</p>`;
+            });
+            document.getElementById("search-results").innerHTML = searchResults;
+        })
+        .catch(error => {
+            console.log(error);
+            alert("An error occurred while searching. Please try again later.");
+        });
+}
